3335c8127dc7b68eedd0c162856c4314445e5fb0
Bruno-Cerqueira <brunocerqueirasd@gmail.com>
2020-09-16 14:34:32 -0300

    Pratice with functions on dart.

---
+++ functions.dart
@@ -0,0 +1,47 @@
+main() {
+  // Basics
+  bool isPar(int number) {
+    return number % 2 == 0;
+  }
+
+  print('2 is par ${isPar(2)}');
+
+  bool isImpar(int number) {
+    return number % 2 != 0;
+  }
+
+  print('3 is impar ${isImpar(3)}');
+
+  // Parameters
+
+  // named arguments
+  showName({String name, String surname}) {
+    return print(name + surname);
+  }
+
+ // positional argument, indicate optional arguments with List []
+ // default value param = value
+  showFullName(String name, [String surname = 'Simpson']) {
+    return print('$name $surname');
+  }
+
+  showFullName('John', 'Black');
+  showFullName('John');
+  showName(name: 'John', surname: 'White');
+
+  // function as params
+
+  ['a', 'b'].forEach((element) => showFullName(element));
+  ['a', 'b'].forEach(showFullName);
+
+  // function on variable
+
+  var message = (message) => message.toUpperCase();
+  print(message('anton'));
+
+  // anonymous function
+
+  List<String> house = ['window', 'door', 'wall', 'roof'];
+  
+  house.forEach((e) => print(e));
+}
